97585a9e242ccc432cf40f4eb90bcdd4ebf76f45
valentinamassalin <valentina.massalind@gmail.com>
2025-07-16 17:21:34 -0300

    Entrega

--- scripts.js
+++ scripts.js
@@ -3,3 +3,23 @@ window.addEventListener('scroll', function() {
   const navbar = document.querySelector('.navbar');
   navbar.classList.toggle('scrolled', window.scrollY > 50);
 });
+
+
+  var tag = document.createElement('script');
+  tag.src = "https://youtu.be/WD92DDi_R-8";
+  document.head.appendChild(tag);
+
+  var player;
+  function onYouTubeIframeAPIReady() {
+    player = new YT.Player('ytVid', {
+      events: {
+        'onReady': (e) => e.target.playVideo()
+      }
+    });
+  }
+
+  document.getElementById('activarAudio').addEventListener('click', () => {
+    player.unMute();
+    player.setVolume(100);
+    player.playVideo();
+  });
